5cb006d8a4584e6239100bc6633eb2e211b7eb8c
pranav-gar <99404869+pranav-gar@users.noreply.github.com>
2022-05-13 21:10:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.img-slide').slick({
+        autoplay: true,
+        slideToShow: 1,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 2000,
+        infinite: true,
+        loop: true,
+    });
+});
---
+++ script.js
@@ -0,0 +1,45 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", ()=>{
+    if(navigator.geolocation){
+        button.innerText = "Allow to detect location";
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        button.innerText = "Your browser not support";
+    }
+});
+
+function onSuccess(position){
+    button.innerText = "Detecting your location...";
+    let {latitude, longitude} = position.coords;
+    let lastString = [5, "e", 7, "b", 2],
+    reverseString = lastString.reverse().join(""),
+    key = concatStrings + reverseString;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${key}`)
+    .then(response => response.json()).then(response =>{
+        let allDetails = response.results[0].components;
+        console.table(allDetails);
+        let {hamlet, road, county, city, country} = allDetails;
+        if(road == undefined || road == "unnamed road"){
+            road = hamlet;
+        }
+        if(city == undefined){
+            city = county;
+        }
+        button.innerText = `${road}, ${city}, ${country}`;
+        key = "";
+    }).catch(()=>{
+        button.innerText = "Something went wrong";
+    });
+}
+
+function onError(error){
+    if(error.code == 1){
+        button.innerText = "You denied the request";
+    }else if(error.code == 2){
+        button.innerText = "Location is unavailable";
+    }else{
+        button.innerText = "Something went wrong";
+    }
+    button.setAttribute("disabled", "true");
+}
